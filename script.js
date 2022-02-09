af8666aa1c69720de691b231a5ac7aefca85ced1
Alex77784 <7509608@gmail.com>
2022-02-09 16:41:39 +0300

    feat:add js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello')
+
+let a = Math.random();
+console.log(a);
+
+let b = Math.random() * 7 + 1;
+console.log(b);
+
+let c = Math.round(Math.random() * 7 + 1);
+console.log(c);
+
+function hello(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+console.log(hello(5, 7))
